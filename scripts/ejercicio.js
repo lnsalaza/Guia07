f469092244243855f6dd47a764e75d443b381e14
lnsalaza <lnsalaza@espol.edu.ec>
2022-06-20 14:42:27 -0500

    mi primer comentario

---
+++ scripts/ejercicio.js
@@ -0,0 +1,26 @@
+
+let ejecutarCodigo = () => {
+    //alert('Hola, mundo!');
+    let element = document.getElementById('titulo1');
+    element.textContent = 'Album de fotos';
+
+    let arreglo = document.getElementsByClassName('text-muted');
+    element = arreglo[1];
+    element.innerHTML = '<span> En este sitio encontrarás un album de fotos inspirado en el snippet de <a href="https://codepen.io/taj1uddin/pen/eYVrLKy">Codepen - Taj Uddin</a>.</span>';
+    
+    arreglo = document.getElementsByTagName('p')
+    arreglo[2].setAttribute('class', 'd-none')
+
+    let arreglo2 = [{url: 'https://images.unsplash.com/photo-1653942786759-f3caff948222?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'camino'},
+                    {url: 'https://images.unsplash.com/photo-1653988235129-842891001e10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'energia'}, 
+                    {url: 'https://images.unsplash.com/photo-1648737963540-306235c8170e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'papá'}
+                ]
+    
+    for 
+
+}
+
+ejecutarCodigo();
+
+
+
